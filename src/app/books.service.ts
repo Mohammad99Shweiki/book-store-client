ebc9c37a9096633889c87e569dc8baf8346676fb
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2020-12-20 15:53:58 +0100

    Refactored and formatted BooksService

--- src/app/books.service.ts
+++ src/app/books.service.ts
@@ -1,25 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Book } from './models/book';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Book} from './models/book';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BooksService {
-  constructor(private http: HttpClient) {}
-
-  private booksUrl = 'https://book-server-pawelblaszczyk.herokuapp.com/';
-  // private booksUrl = 'http://localhost:3000/';
+  constructor(private http: HttpClient) {
+  }
 
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    headers: new HttpHeaders({'Content-Type': 'application/json'}),
   };
 
-  getLimitedBooks(limit: number, skip: number): Observable<Book[]> {
-    return this.http.get<Book[]>(this.booksUrl + `limitskip/${limit}/${skip}`);
+  getBooksWithLimitAndSkip(limit: number, skip: number): Observable<Book[]> {
+    return this.http.get<Book[]>(environment.serverUrL + `limitSkip/${limit}/${skip}`);
   }
+
   getBook(id: number): Observable<Book> {
-    return this.http.get<Book>(this.booksUrl + `getById/${id}`);
+    return this.http.get<Book>(environment.serverUrL + `getBookById/${id}`);
   }
 }
