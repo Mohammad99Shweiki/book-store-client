023093902346e9bee90646fbc4e4572a6043b961
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-01-02 15:16:32 +0100

    detailsMapping helper

---
+++ src/app/helpers/detailsMapping/detailsMapping.ts
@@ -0,0 +1,12 @@
+import {DetailMapping} from '../../models/detailMapping';
+
+export const detailsMapping: Array<DetailMapping> = [
+  {key: 'author', label: 'Autor'},
+  {key: 'title', label: 'Tytuł'},
+  {key: 'genre', label: 'Gatunek'},
+  {key: 'publication', label: 'Data publikacji'},
+  {key: 'ISBN', label: 'ISBN'},
+  {key: 'bestseller', label: 'Bestseller'},
+  {key: 'new', label: 'Nowość'},
+  {key: 'id', label: 'Numer identyfikacyjny'}
+];
