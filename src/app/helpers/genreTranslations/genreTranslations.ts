bfaeed5ca748b0ac432dc8e63444c5126719875c
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-01-02 15:42:53 +0100

    Genre translations added

---
+++ src/app/helpers/genreTranslations/genreTranslations.ts
@@ -0,0 +1,18 @@
+export const genreTranslations: object = {
+  Action: 'Akcja',
+  Crime: 'Kryminał',
+  Drama: 'Dramat',
+  Comedy: 'Komedia',
+  Mystery: 'Tajmeniczy',
+  Romance: 'Romans',
+  Thriller: 'Thriller',
+  Children: 'Familijny',
+  Fantasy: 'Fantasy',
+  Musical: 'Musical',
+  Adventure: 'Przygodowy',
+  Documentary: 'Dokumentalny',
+  Animation: 'DLa dzieci',
+  Horror: 'Horror',
+  Western: 'Western',
+  War: 'Wojenny'
+};
