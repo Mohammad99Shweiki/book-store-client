b81cd43ecfc4b2691d9e5cd3e5c1b62203cf6552
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-01-02 15:49:08 +0100

    Added default if translation is missing

--- src/app/helpers/genreTranslations/genreTranslations.ts
+++ src/app/helpers/genreTranslations/genreTranslations.ts
@@ -14,5 +14,6 @@ export const genreTranslations: object = {
   Animation: 'DLa dzieci',
   Horror: 'Horror',
   Western: 'Western',
-  War: 'Wojenny'
+  War: 'Wojenny',
+  'Film-Noir' : 'Starodawny'
 };
