c3c2dfd6803fa3610a1633b49018f42e00e431c2
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-01-06 12:40:16 +0100

    Changed angular config and cleaned up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title: string = 'book-shop';
-}
+export class AppComponent {}
