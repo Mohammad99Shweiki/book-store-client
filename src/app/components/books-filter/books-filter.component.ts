744b04b50083d7602f6c984c1dbf353354d4b54b
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-01-10 11:02:00 +0100

    Books-filter styling completed

--- src/app/components/books-filter/books-filter.component.ts
+++ src/app/components/books-filter/books-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faCaretUp, faCaretDown, IconDefinition} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-books-filter',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books-filter.component.css']
 })
 export class BooksFilterComponent implements OnInit {
-
-  constructor() { }
+  genres: Array<string> = ['Humor', 'Romance', 'Action', 'Adventure', 'Classic', 'Graphic Novel', 'Fantasy', 'Crime', 'Historical Fiction', 'Horror', 'Literary Fiction', 'Sci-Fi', 'Short Stories',
+    'Thriller', 'Biography', 'History', 'Poetry'];
+  faCaretUp: IconDefinition = faCaretUp;
+  faCaretDown: IconDefinition = faCaretDown;
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  test(event: Event): void {
+    console.log(event);
+  }
 }
