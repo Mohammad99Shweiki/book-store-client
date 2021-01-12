6e5e54f98d3ca3f4ae70883780c04982b6e1a668
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-01-12 21:45:06 +0100

    Main page WIP

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BooksService} from '../../services/books/books.service';
+import {Book} from '../../models/book';
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
+  bestsellers: Array<Book> = [];
+  sales: Array<Book> = [];
+  new: Array<Book> = [];
+  booksLoaded: number = 0;
 
-  constructor() { }
+  constructor(private booksService: BooksService) {
+  }
 
   ngOnInit(): void {
+    this.getBestsellers();
+    this.getNew();
+    this.getSales();
+  }
+
+  getBestsellers(): void {
+    this.booksService.getBestsellers().subscribe((books: Array<Book>) => {
+      this.bestsellers = books;
+      this.booksLoaded += books.length;
+    });
+  }
+
+  getSales(): void {
+    this.booksService.getSales().subscribe((books: Array<Book>) => {
+      this.sales = books;
+      this.booksLoaded += books.length;
+    });
+  }
+
+  getNew(): void {
+    this.booksService.getNew().subscribe((books: Array<Book>) => {
+      this.new = books;
+      this.booksLoaded += books.length;
+    });
   }
 
 }
