9030d26ab9efd5bc1a5bb7644f94797bcbfaf463
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-01-27 20:13:32 +0100

    Send contact message to DB

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import {ContactService} from '../../services/contact/contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -9,13 +10,28 @@ import {FormControl, Validators} from '@angular/forms';
 export class ContactComponent implements OnInit {
   emailControl: FormControl = new FormControl('', [Validators.required, Validators.email]);
   messageControl: FormControl = new FormControl('', [Validators.required]);
+  sendingError: boolean = false;
 
-  constructor() { }
+  constructor(private contactService: ContactService) {
+  }
 
   ngOnInit(): void {
   }
 
-  validateControls(): void {
+  sendContactData(): void {
+    this.sendingError = false;
+    const data: { contactEmail: string, messageText: string } = {
+      contactEmail: this.emailControl.value,
+      messageText: this.messageControl.value
+    };
+    this.contactService.sendContactMessage(data).subscribe((result: boolean) => {
+      if (result) {
+        this.emailControl.reset();
+        this.messageControl.reset();
+      } else {
+        this.sendingError = true;
+      }
+    });
   }
 
 }
---
+++ src/app/services/contact/contact.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  constructor(private http: HttpClient) { }
+
+  sendContactMessage(data): Observable<boolean> {
+    return this.http.post<boolean>(environment.serverUrL + 'sendContact', data);
+  }
+}
