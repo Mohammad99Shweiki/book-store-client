a4ec8ae9c18d54254a0e0820023be64c891be576
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-01-28 21:47:39 +0100

    Login form

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
+  @Input() onGoingRequest: boolean;
+
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+
+  }
 }
