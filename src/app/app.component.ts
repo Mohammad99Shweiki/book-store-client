543bc180a5736d99ccf128c0fa1de876e034c8fe
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-02-02 20:52:22 +0100

    Set scroll top component bottom position if footer is in view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   sidebarOpened: boolean = false;
+  @ViewChild('footer') footer: ElementRef;
+  footerHeight: number;
+
+  constructor(private ref: ChangeDetectorRef) {
+  }
+
+  ngAfterViewInit(): void {
+    this.setFooterHeight();
+    this.ref.detectChanges();
+  }
+
+  setFooterHeight(): void {
+    this.footerHeight = this.footer.nativeElement.getBoundingClientRect().height;
+  }
+
+  @HostListener('window:resize', [])
+  onResize(): void {
+    this.setFooterHeight();
+  }
 }
--- src/app/components/scroll-top/scroll-top.component.ts
+++ src/app/components/scroll-top/scroll-top.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {Component, HostListener, Input, OnInit} from '@angular/core';
 import {faArrowUp, IconDefinition} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -7,9 +7,13 @@ import {faArrowUp, IconDefinition} from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./scroll-top.component.css']
 })
 export class ScrollTopComponent implements OnInit {
+  @Input() footerHeight: number;
   faArrowUp: IconDefinition = faArrowUp;
   scrollOpacity: number = 0;
-  constructor() { }
+  bottomPosition: number = 40;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
@@ -19,6 +23,20 @@ export class ScrollTopComponent implements OnInit {
     const offset: number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
     const windowHeight: number = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     this.scrollOpacity = offset > windowHeight * 0.7 ? 1 : 0;
+    this.setUpBottomPosition();
+  }
+
+  @HostListener('window:resize', [])
+  onWindowResize(): void {
+    this.setUpBottomPosition();
+  }
+
+  setUpBottomPosition(): void {
+    const offset: number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const documentHeight: number = document.documentElement.scrollHeight;
+    const windowHeight: number = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    this.bottomPosition = documentHeight - offset - windowHeight < this.footerHeight ?
+      ((documentHeight - offset - windowHeight - this.footerHeight) * - 1) + 40 : 40;
   }
 
   scrollTop(): void {
