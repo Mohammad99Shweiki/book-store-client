4ad0dcf26df53fd128f95f409e54a9f50b6c674a
abdalazeezS <abdalazeezshahateet@gmail.com>
2024-05-24 22:33:16 +0300

    add validation in see more button

--- src/app/components/book-overview/book-overview.component.ts
+++ src/app/components/book-overview/book-overview.component.ts
@@ -5,6 +5,7 @@ import { CartService } from '@/services/cart/cart.service';
 import { AuthService } from '@/services/auth/auth.service';
 import { InfoDialogComponent } from '../info-dialog/info-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-overview',
@@ -18,17 +19,34 @@ export class BookOverviewComponent implements OnInit {
   constructor(
     private cartService: CartService,
     private authService: AuthService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private router: Router
   ) {
-    this.authService.loggedInChange.subscribe(res => this.loggedIn = res);
+    this.authService.checkUserData().subscribe(res => {
+      this.loggedIn = res === true;
+    })
   }
 
   ngOnInit(): void {
+    this.authService.loggedInChange.subscribe(res => {
+      this.loggedIn = res;
+    })
   }
 
   addToBasket(id: string): void {
     if (this.loggedIn) {
       this.cartService.addProductToCart(id);
+    } else {
+      this.dialog.open(InfoDialogComponent, {
+        width: '400px',
+        data: { message: 'Please login to perform this process' }
+      })
+    }
+  }
+
+  showDetails(isbn: string) {
+    if (this.loggedIn) {
+      this.router.navigate([`/book/${isbn}`]);
     } else {
       this.dialog.open(InfoDialogComponent, {
         width: '400px',
